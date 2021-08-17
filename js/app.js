12d1f6394f37e2e49733e64c929a0543269c15cf
Рафаэль Насыбуллин <crazynihonjin97@gmail.com>
2021-08-18 02:57:11 +0300

    17/8/2021

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
