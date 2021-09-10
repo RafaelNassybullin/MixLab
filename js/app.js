d6d0c32bd6776f3c29055b0d0ccabb0157e07dc7
Рафаэль Насыбуллин <crazynihonjin97@gmail.com>
2021-09-11 00:42:26 +0300

    19/8/2021

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-var swiper = new Swiper(".mySwiper", {
+const burger = document.querySelector('.burger');
+const close = document.querySelector('.close');
+
+
+let swiper = new Swiper(".mySwiper", {
     slidesPerView: 'auto',
     centeredSlides: true,
     grabCursor: true,
@@ -8,4 +12,19 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     },
 });
+
+let tl = gsap.timeline();
+
+window.innerWidth<1050 ? burger.addEventListener('click', () => {
+    if (tl.reversed()) {
+        tl.play();
+    } else {
+        tl.to('.navbar ul', {bottom: '0', display: 'flex'}, '-=.3')
+            .to('.navbar ul li', {opacity: 1, pointerEvents: 'all', stagger: .2}, '0.3')
+    }
+}):''
+
+window.innerWidth<1050 ?close.addEventListener('click', () => {
+    tl.reverse();
+}):'';
 
